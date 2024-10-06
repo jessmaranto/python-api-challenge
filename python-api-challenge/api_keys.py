418fd648194dbc345d6392cc839024cd1b603b03
jessmaranto <jessicamaranto@msn.com>
2024-10-06 12:35:48 -0600

    Update api_keys.py

--- python-api-challenge/api_keys.py
+++ python-api-challenge/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "460339cd0a48cff1ea3297089c827b0e"
+weather_api_key = ""
 
 # Geoapify API Key
-geoapify_key = "2e140fefd2c1495cb279c661d825f740"
+geoapify_key = ""
